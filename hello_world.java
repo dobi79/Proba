7ed9ec0689c01ea20a10ca3920d5e5eb68b15cd4
petra95 <antalicspetra@gmail.com>
2016-07-07 14:45:11 +0200

    ujkomment

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
 	//uj sor hozzadva
+	//Petra írt bele :D
         System.out.println("Hello, World");
     }
 
